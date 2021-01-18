f75af967f58e99b4283ed4ba4cf8b3bc96bca18c
Celticcow <gregory.dunlap@gmail.com>
2021-01-18 15:51:42 -0600

    class works

--- cmdb_query.py
+++ cmdb_query.py
@@ -24,6 +24,50 @@ class cmdb_query(object):
         self.fqdn = "N/A"
         self.location = "N/A"
         self.support_group = "N/A"
-        self.support_group.manager = "N/A"
+        self.support_group_manager = "N/A"
+
+    #accessors
+    def get_ip(self):
+        return(self.ip)
     
+    def get_sys_class_name(self):
+        return(self.sys_class_name)
     
+    def get_name(self):
+        return(self.name)
+    
+    def get_fqdn(self):
+        return(self.fqdn)
+    
+    def get_location(self):
+        return(self.location)
+    
+    def get_support_group(self):
+        return(self.support_group)
+    
+    def get_support_group_manager(self):
+        return(self.support_group_manager)
+    
+    #modifiers
+    def set_ip(self, ip):
+        self.ip = ip
+    
+    def set_sys_class_name(self, sys_class_name):
+        self.sys_class_name = sys_class_name
+    
+    def set_name(self, name):
+        self.name = name
+    
+    def set_fqdn(self, fqdn):
+        self.fqdn = fqdn
+    
+    def set_location(self, location):
+        self.location = location
+    
+    def set_support_group(self, support_group):
+        self.support_group = support_group
+    
+    def set_support_group_manager(self, support_group_manager):
+        self.support_group_manager = support_group_manager
+    
+#end of class
