62785c088aa7a3e1e1de636ce964b0535f4cac69
Celticcow <gregory.dunlap@gmail.com>
2021-01-19 12:14:37 -0600

    class model

--- cmdb_query.py
+++ cmdb_query.py
@@ -26,6 +26,14 @@ class cmdb_query(object):
         self.support_group = "N/A"
         self.support_group_manager = "N/A"
 
+        http_proxy = "199.82.243.100:3128"
+        https_proxy = "199.82.243.100:3128"
+
+        self.proxyDict = {
+            "http" : http_proxy,
+            "https" : https_proxy
+        }
+
     #accessors
     def get_ip(self):
         return(self.ip)
@@ -48,6 +56,10 @@ class cmdb_query(object):
     def get_support_group_manager(self):
         return(self.support_group_manager)
     
+    def print_cmdb(self):
+        out_str = "{name : " + self.name + ", ip : " + self.ip + ", fqdn : " + self.fqdn + ", location : " + self.location + ", Sys_Class_Name : " + self.sys_class_name + ", Support Group : " + self.support_group + ", Support Group Manager : " + self.support_group_manager + "}"
+        print(out_str)
+
     #modifiers
     def set_ip(self, ip):
         self.ip = ip
@@ -70,4 +82,63 @@ class cmdb_query(object):
     def set_support_group_manager(self, support_group_manager):
         self.support_group_manager = support_group_manager
     
+    #worker functions
+    def query_cmdb(self):
+        if(self.ip == "127.0.0.1"):
+            return
+        else:
+            #run query.
+            fields = '&fields=sys_class_name,name,fqdn,sys_id,location,ip_address,support_group,support_group.manager&display_value=true'
+
+            url = 'https://pdsmdev08.service-now.com/api/x_hclfe_cmdb_api/hardware/by_ip?ip_address=' + self.ip + fields
+
+            key = {}
+            with open('key.json', 'r') as f:
+                key = json.load(f)
+
+            #print(key['usr'])
+            #print(key['pwd'])
+
+            headers = {"Accept" : "application/json"}
+
+            # Do the HTTP request
+            response = requests.get(url, auth=(key['usr'], key['pwd']), headers=headers, proxies=self.proxyDict)
+
+            ####
+            if(response.status_code != 200):
+                print("Not return code 200 ", end=" ")
+                print(response.status_code)
+                #print("Header returned ", end=" ")
+                #print(response.headers)
+                #print("Error Response ", end=" ")
+                #print("{}")
+                ##print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
+                #exit()
+                self.name = "N/A"
+                self.fqdn = "N/A"
+                self.location = "N/A"
+                self.sys_class_name = "N/A"
+                self.support_group = "N/A"
+                self.support_group_manager = "N/A"
+            else:
+                #print("Status :", response.status_code, "headers: ", response.headers)
+                #print("Response : ", end="\n")
+                #print(response.json())
+                tmp = response.json()
+                #print("---------------------------------\n")
+                #print(tmp['result']['Hardware Details'][0]['name'])
+                #print(tmp['result']['Hardware Details'][0]['location'])
+                #print(tmp['result']['Hardware Details'][0]['support_group'])
+                #print(tmp['result']['Hardware Details'][0]['support_group.manager'])
+                #print("---------------------------------\n")
+                #print("\n\n")
+                #print("Cookies:", response.cookies)
+
+                self.name = tmp['result']['Hardware Details'][0]['name']
+                self.fqdn = tmp['result']['Hardware Details'][0]['fqdn']
+                self.location = tmp['result']['Hardware Details'][0]['location']
+                self.sys_class_name = tmp['result']['Hardware Details'][0]['sys_class_name']
+                self.support_group = tmp['result']['Hardware Details'][0]['support_group']
+                self.support_group_manager = tmp['result']['Hardware Details'][0]['support_group.manager']
+
 #end of class
--- test2.py
+++ test2.py
@@ -9,61 +9,19 @@ def main():
     debug = 1
     print("test 2")
 
-    ip_addr = "204.135.8.187"
+    q1 = cmdb_query()
+    q1.set_ip("199.81.216.26")
+    q1.query_cmdb()
 
-    fields = '&fields=sys_class_name,name,fqdn,sys_id,location,ip_address,support_group,support_group.manager&display_value=true'
-
-    url = 'https://pdsmdev08.service-now.com/api/x_hclfe_cmdb_api/hardware/by_ip?ip_address=' + ip_addr + fields
-
-    key = {}
-    with open('key.json', 'r') as f:
-        key = json.load(f)
-
-    print(key['usr'])
-    print(key['pwd'])
-
-    headers = {"Accept" : "application/json"}
-
-    http_proxy = "199.82.243.100:3128"
-    https_proxy = "199.82.243.100:3128"
-
-    proxyDict = {
-        "http" : http_proxy,
-        "https" : https_proxy
-    }
-
-
-    # Do the HTTP request
-    response = requests.get(url, auth=(key['usr'], key['pwd']), headers=headers, proxies=proxyDict)
-
-    q = cmdb_query()
-
+    print(q1.get_fqdn())
+    print(q1.get_sys_class_name())
     
+    q1.print_cmdb()
 
-    if(response.status_code != 200):
-        print("Not return code 200 ", end=" ")
-        print(response.status_code)
-        print("Header returned ", end=" ")
-        print(response.headers)
-        print("Error Response ", end=" ")
-        print("{}")
-        ##print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
-        exit()
+    q2 = cmdb_query("199.81.212.9")
+    q2.query_cmdb()
 
-    print("Status :", response.status_code, "headers: ", response.headers)
-    print("Response : ", end="\n")
-    print(response.json())
-    tmp = response.json()
-    print("---------------------------------\n")
-    print(tmp['result']['Hardware Details'][0]['name'])
-    print(tmp['result']['Hardware Details'][0]['location'])
-    print(tmp['result']['Hardware Details'][0]['support_group'])
-    print(tmp['result']['Hardware Details'][0]['support_group.manager'])
-    print("---------------------------------\n")
-    print("\n\n")
-    print("Cookies:", response.cookies)
-
-
+    q2.print_cmdb()
 
 if __name__ == "__main__":
     main()
